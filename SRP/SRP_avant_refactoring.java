9df458f86d46c2c3484beef5a0a32b5e8e481e2b
oceannemj <judithoceanne12@gmail.com>
2025-12-26 17:24:11 +0100

    srp

---
+++ SRP/SRP_avant_refactoring.java
@@ -0,0 +1,42 @@
+package ict301.solid.srp;
+public class Book {
+private String title;
+private String author;
+private String content;
+
+public Book(String title, String author, String content) {
+   this.title = title;
+   this.author = author;
+   this.content = content; }
+
+ //  Gérer les données du livre
+   public String getTitle() { return title; }
+   public String getAuthor() { return author; }
+   public String getContent() { return content; }
+
+ //  Afficher le livre (présentation)
+   public void printToScreen() {
+   System.out.println("Titre: " + title);
+   System.out.println("Auteur: " + author);
+   System.out.println("Contenu: " + content); }
+
+//  Sauvegarder le livre (persistance)
+   public void saveToDatabase() {
+   System.out.println("Sauvegarde du livre '" + title + "' en base de données..."); }
+
+//  (logique métier)
+   public void emprunter(String lecteur) {
+   System.out.println("Emprunt du livre '" + title + "' par " + lecteur); } }
+
+
+
+
+// Utilisation de la classe Book
+   public class Main {
+   public static void main(String[] args) {
+   Book book = new Book("Les principes SOLID", "Etudiants de M1-GL");
+   book.printToScreen();
+   book.saveToDatabase();
+   book.emprunter("Marcial");
+   }
+ } 
