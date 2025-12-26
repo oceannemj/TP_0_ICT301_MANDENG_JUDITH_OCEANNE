4032ed7589c1c3925895bf058e8d0f9e1a8e68bb
oceannemj <judithoceanne12@gmail.com>
2025-12-26 17:55:05 +0100

    ocp

---
+++ OCP/AreaCalculator.java
@@ -0,0 +1,45 @@
+
+package OCP;
+
+class Rectangle {
+    private double width;
+    private double height;
+    
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    public double getWidth() {
+        return width;
+    }
+    
+    public double getHeight() {
+        return height;
+    }
+}
+
+class Circle {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    public double getRadius() {
+        return radius;
+    }
+}
+
+public class AreaCalculator {
+    public double calculateArea(Object shape) {
+        if (shape instanceof Rectangle) {
+            Rectangle rectangle = (Rectangle) shape;
+            return rectangle.getWidth() * rectangle.getHeight();
+        } else if (shape instanceof Circle) {
+            Circle circle = (Circle) shape;
+            return Math.PI * circle.getRadius() * circle.getRadius();
+        }
+        throw new IllegalArgumentException("Unknown shape");
+    }
+}
---
+++ OCP/OCP_apres_refactoring.java
@@ -0,0 +1,46 @@
+package OCP;
+
+interface Shape {
+    double calculateArea();
+}
+
+class Rectangle implements Shape {
+    private double width;
+    private double height;
+    
+    public Rectangle(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+    
+    @Override
+    public double calculateArea() {
+        return width * height;
+    }
+}
+
+class Circle implements Shape {
+    private double radius;
+    
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    
+    @Override
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+}
+
+class AreaCalculator2 {
+    public double calculateArea(Shape shape) {
+        return shape.calculateArea();
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        Shape shape = new Rectangle(4, 3);
+        System.out.println("Area = '" + shape.calculateArea() + "'");
+    }
+}
